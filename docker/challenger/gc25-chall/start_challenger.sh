b5948ab6222cd2e18fb54d6bacf0d49ca77a0c4e
Alessandro Cortese <ale.cortese99@gmail.com>
2025-06-12 16:37:31 +0200

    Try Commmit
    Co-Authored-By: Luca Martorelli <92364829+MartorelliLuca@users.noreply.github.com>

---
+++ docker/challenger/gc25-chall/start_challenger.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker image load -i gc25cdocker.tar
+docker run -p 8866:8866 --network stream_processing_network --name=challenger --volume ./data:/data micro-challenger:latest 0.0.0.0:8866 /data 
---
+++ docker/start_challenger.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd challenger/gc25-chall
+
+./start_challenger.sh
